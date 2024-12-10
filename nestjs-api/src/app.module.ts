7a03021cae227a2828a8ab2f69128ac22cec02b7
Jônathas Bonfim <jonathas.santos.bonfim@gmail.com>
2024-12-09 21:27:12 -0300

    nestjs/routes

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -3,13 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MapsModule } from './maps/maps.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RoutesModule } from './routes/routes.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     MapsModule,
+    RoutesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
